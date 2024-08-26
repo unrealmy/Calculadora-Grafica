d1f6100ac1297c0dc7c4ce869ec840971b592990
Charles A. Ferreyra <74272417+unrealmy@users.noreply.github.com>
2024-08-26 14:42:21 -0300

    Add files via upload

---
+++ src/miprimercalculadoradecafb2023/CalculadoraMejoradaPorChatGPT.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package miprimercalculadoradecafb2023;
+
+/**
+ *
+ * @author usuario
+ */
+//public class CalculadoraMejoradaPorChatGPT {
+    
+//}
+
+/*import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Calculadora extends JFrame implements ActionListener {
+    private JLabel pantallaCalculadora;
+    private StringBuilder numeroActual;
+    private int num1, num2;
+    private String operador;
+
+    public Calculadora() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        JPanel jPanel1 = new JPanel();
+        pantallaCalculadora = new JLabel();
+        JButton btnC = new JButton("C");
+        JButton btnPorcentaje = new JButton("%");
+        JButton btnDividir = new JButton("/");
+        JButton btnAC = new JButton("AC");
+        JButton btn8 = new JButton("8");
+        JButton btn9 = new JButton("9");
+        JButton btnMultiplicar = new JButton("X");
+        JButton btn7 = new JButton("7");
+        JButton btn5 = new JButton("5");
+        JButton btn6 = new JButton("6");
+        JButton btnRestar = new JButton("-");
+        JButton btn4 = new JButton("4");
+        JButton btn2 = new JButton("2");
+        JButton btn3 = new JButton("3");
+        JButton btnSumar = new JButton("+");
+        JButton btn1 = new JButton("1");
+        JButton btnPunto = new JButton(".");
+        JButton btnIgual = new JButton("=");
+        JButton btn0 = new JButton("0");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
+
+        pantallaCalculadora.setBackground(new java.awt.Color(255, 255, 255));
+        pantallaCalculadora.setFont(new java.awt.Font("Times New Roman", 0, 24));
+        pantallaCalculadora.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        pantallaCalculadora.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 51, 255)));
+        pantallaCalculadora.setOpaque(true);
+
+        btnC.setBackground(new java.awt.Color(255, 0, 0));
+        btnC.addActionListener(this);
+
+        btnPorcentaje.setBackground(new java.awt.Color(102, 102, 102));
+        btnPorcentaje.addActionListener(this);
+
+        btnDividir.setBackground(new java.awt.Color(102, 102, 102));
+        btnDividir.addActionListener(this);
+
+        btnAC.setBackground(new java.awt.Color(255, 0, 0));
+        btnAC.addActionListener(this);
+
+        btn8.setBackground(new java.awt
+*/
